4a00e62099b2feca7d26c50924802b3b4ff28d86
akshay <78022889+akshayp2@users.noreply.github.com>
2021-12-21 00:22:05 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,60 @@
+import string
+import stopwords
 from flask import Flask,render_template,request
-from tweets import Twitterclient,TweetAnalyzer
+from tweets import Twitterclient
+from collections import Counter
 app = Flask(__name__)
 tweet=""
 @app.route("/",methods=['POST','GET'])
 def home():
     if request.method=='POST':
-        username = request.form['username'] 
-        count = request.form.get('noOfTweets')
-        twitter_client1 = Twitterclient()
-        # tweet_analyzer = TweetAnalyzer()
-        api = twitter_client1.get_twitter_client_api()
-        tweets = api.user_timeline(screen_name=username,count=count)
-        return render_template("index.html",tweets=tweets)
+        username = request.form['username']
+        print('user name ---->'+username)
+        if len(username)!=0:
+            count = request.form.get('noOfTweets')
+            twitter_client1 = Twitterclient()
+            api = twitter_client1.get_twitter_client_api()
+            tweets = api.user_timeline(screen_name=username,count=count)
+            emotionlist, w = analyseEmotions(tweets=tweets)
+            labels = []
+            for label in w.keys():
+                labels.append(label)
+            data = [] 
+            for val in w.values():
+                data.append(val)
+            print(tweets)
+            return render_template("index.html",tweets=tweets,labels=labels,data=data,username=username)
+        else:
+            return render_template("index.html",tweets=[],labels=[],data=[],username=username)
     else:
         return render_template("index.html",tweet='no tweet available')
 
+
+def analyseEmotions(tweets):
+    text = ' '.join([str(elem) for elem in tweets])
+    lower_case = text.lower()
+    cleaned_text = lower_case.translate(str.maketrans('','',string.punctuation))
+
+    token = cleaned_text.split()
+    stop_words = stopwords.stop_words
+    final_word = []
+    for word in token:
+        if word not in stop_words:
+            final_word.append(word)
+    emotion_list = []
+
+    with open('./static/emotions.csv','r',encoding='latin-1') as file:
+        for line in file:
+            clear_line = line.replace('\n','').replace("'",'').strip()
+            word,emotion = clear_line.split(',')
+            if word in final_word:
+                emotion_list.append(emotion)
+    print(emotion_list)
+    w = Counter(emotion_list)
+    print(w)
+    return emotion_list,w
+
+
 if __name__=='__main__':
-    # df = tweet_analyzer.tweets_to_data_frame(tweets)
-    # df.to_csv('test.csv')
-    # print(df.head(5))
-    app.run()
+    app.run(debug=True)
     
--- tweets.py
+++ tweets.py
@@ -1,5 +1,4 @@
 from tweepy import API
-from tweepy import Cursor
 from tweepy import OAuthHandler
 import pandas as pd
 
@@ -14,12 +13,6 @@ class Twitterclient():
     def get_twitter_client_api(self):
         return self.twitter_client
 
-    def get_user_timeline_tweets(self,num_tweets):
-        tweets = []
-        for tweet in Cursor(self.twitter_client.user_timeline,id=self.twitter_user).items(num_tweets):
-            tweets.append(tweet)
-        return tweets
-
 class TwitterAuthenticator():
     def authenticate_twitter_app(self):
         auth = OAuthHandler(twitter_credential.CONSUMER_KEY,twitter_credential.CONSUMER_SECRET)
@@ -31,12 +24,3 @@ class TweetAnalyzer():
     def tweets_to_data_frame(self,tweets):
         df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
         return df
-
-# if __name__ == "__main__":
-#    twitter_client = Twitterclient()
-#    tweet_analyzer = TweetAnalyzer()
-#    api = twitter_client.get_twitter_client_api()
-#    tweets = api.user_timeline(screen_name="elonmusk",count="10")
-#    df = tweet_analyzer.tweets_to_data_frame(tweets)
-#    df.to_csv('test.csv')
-#    print(df.head(5))
